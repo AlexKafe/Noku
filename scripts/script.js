163b5d007042eb1febf6ad06875d5ff9d9c0c691
Alex Kafé <alexkafe18@gmail.com>
2023-09-14 12:41:19 -0300

    versao mobile first finalizada

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+function OtoTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = "block"
+    }
+} 
+function clickmenu() {
+    if (menu.style.display == "block") {
+        menu.style.display = "none"
+    }
+    else {
+        menu.style.display = "block"
+    }
+}
